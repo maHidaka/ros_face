001e6156ba655164905f98851b2cd58882560670
maHidaka <mhdk816@gmail.com>
2021-03-31 11:32:47 +0900

    整理

--- ros_face_driver/src/serial_driver.cpp
+++ ros_face_driver/src/serial_driver.cpp
@@ -8,29 +8,29 @@
 
 int open_serial(const char *device_name)
 {
-    int fd1 = open(device_name, O_RDWR | O_NOCTTY | O_NONBLOCK);
-    fcntl(fd1, F_SETFL, 0);
+    int fd = open(device_name, O_RDWR | O_NOCTTY | O_NONBLOCK); //デバイス名, 読み書き権限｜制御端末にならない設定｜可能ならば非停止
+    fcntl(fd, F_SETFL, 0);                                      //ファイルディスクリプタfdのファイルステータスフラグを0に設定
     //load configuration
-    struct termios conf_tio;
-    tcgetattr(fd1, &conf_tio);
+    struct termios conf_tio;  //termiosの構造体宣言
+    tcgetattr(fd, &conf_tio); //termptrが指すメモリー位置に端末の制御情報を保管
     //set baudrate
-    speed_t BAUDRATE = B1000000;
+    speed_t BAUDRATE = B115200; //ボーレート
     cfsetispeed(&conf_tio, BAUDRATE);
     cfsetospeed(&conf_tio, BAUDRATE);
-    //non canonical, non echo back
-    conf_tio.c_lflag &= ~(ECHO | ICANON);
-    //non blocking
-    conf_tio.c_cc[VMIN] = 0;
-    conf_tio.c_cc[VTIME] = 0;
+    //非カノニカルモード
+    conf_tio.c_lflag &= ~(ECHO | ICANON); //ICANONはカノニカルモード
+    //ポーリングリード
+    conf_tio.c_cc[VMIN] = 0;  //最小文字数
+    conf_tio.c_cc[VTIME] = 0; //タイムアウト時間
     //store configuration
-    tcsetattr(fd1, TCSANOW, &conf_tio);
-    return fd1;
+    tcsetattr(fd, TCSANOW, &conf_tio);
+    return fd;
 }
 
-int fd1;
+int fd;
 void serial_callback(const std_msgs::String &serial_msg)
 {
-    int rec = write(fd1, serial_msg.data.c_str(), serial_msg.data.size());
+    int rec = write(fd, serial_msg.data.c_str(), serial_msg.data.size());
     if (rec >= 0)
         printf("send:%s\n", serial_msg.data.c_str());
     else
@@ -50,20 +50,20 @@ int main(int argc, char **argv)
     //Subscriber
     ros::Subscriber serial_sub = n.subscribe("Serial_out", 10, serial_callback);
 
-    char device_name[] = "/dev/ttyUSB0";
-    fd1 = open_serial(device_name);
-    if (fd1 < 0)
+    char device_name[] = "/dev/ttyACM0";
+    fd = open_serial(device_name);
+    if (fd < 0)
     {
         ROS_ERROR("Serial Fail: cound not open %s", device_name);
         printf("Serial Fail\n");
         ros::shutdown();
     }
 
-    ros::Rate loop_rate(200);
+    ros::Rate loop_rate(10);
     while (ros::ok())
     {
         char buf[256] = {0};
-        int recv_data = read(fd1, buf, sizeof(buf));
+        int recv_data = read(fd, buf, sizeof(buf));
         if (recv_data > 0)
         {
             printf("recv:%03d %s\n", recv_data, buf);
