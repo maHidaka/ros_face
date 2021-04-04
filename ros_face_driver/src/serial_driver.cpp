b8b16bd5c49831290e4a3d1fc42355c280738ba9
maHidaka <m.hdk816@gmail.com>
2021-04-04 15:23:05 +0900

    driver追加

--- ros_face_driver/src/serial_driver.cpp
+++ ros_face_driver/src/serial_driver.cpp
@@ -63,10 +63,10 @@ int main(int argc, char **argv)
         ros::shutdown(); //ノードの停止
     }
 
-    ros::Rate loop_rate(100);
+    ros::Rate loop_rate(200);
     while (ros::ok())
     {
-        char buf[34] = {0};                         //読み込むサイズ(改行文字含む)+1byteを用意
+        char buf[256] = {0};                         //読み込むサイズ(改行文字含む)+1byteを用意
         int recv_data = read(fd, buf, sizeof(buf)); //handleにつながっているファイルからnバイト読み込みbufに格納
         if (recv_data > 0)
         {
