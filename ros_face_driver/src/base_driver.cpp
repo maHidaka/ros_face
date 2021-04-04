0ddb8381a98cd1f0017a8dcfab67b835b88b4bca
maHidaka <m.hdk816@gmail.com>
2021-04-04 18:04:27 +0900

    launch追加、名前の整理

--- ros_face_driver/src/base_driver.cpp
+++ ros_face_driver/src/base_driver.cpp
@@ -1,26 +1,26 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-class RosFaceDriver{
+class BaseDriver{
 	private:
 		ros::NodeHandle n;
 		ros::Publisher pub;
 		ros::Subscriber sub;
 		std_msgs::String send_cmd;
 	public:
-		RosFaceDriver();
+		BaseDriver();
 		void Publication(std_msgs::String buf);
 		void Callback(const std_msgs::String &cmd);
 };
 
-RosFaceDriver::RosFaceDriver()
+BaseDriver::BaseDriver()
 {
 	pub = n.advertise<std_msgs::String>("Serial_out", 10);
-	sub = n.subscribe("face_cmd", 10, &RosFaceDriver::Callback, this);
+	sub = n.subscribe("face_cmd", 10, &BaseDriver::Callback, this);
 }
 
 
-void RosFaceDriver::Publication(std_msgs::String buf)
+void BaseDriver::Publication(std_msgs::String buf)
 {
 	send_cmd.data = buf.data;
     printf("pub:%s\n", send_cmd.data.c_str());
@@ -28,18 +28,18 @@ void RosFaceDriver::Publication(std_msgs::String buf)
 }
 
 
-void RosFaceDriver::Callback(const std_msgs::String &cmd)
+void BaseDriver::Callback(const std_msgs::String &cmd)
 {
 	printf("sub:%s\n", cmd.data.c_str());
-    RosFaceDriver::Publication(cmd);
+    BaseDriver::Publication(cmd);
 }
 
 
 int main(int argc, char**argv)
 {
-	ros::init(argc, argv, "ros_face_driver");
+	ros::init(argc, argv, "base_driver");
 	
-	RosFaceDriver ros_face_driver;
+	BaseDriver base_driver;
 
 	ros::Rate loop_rate(100);
 	while(ros::ok()){
