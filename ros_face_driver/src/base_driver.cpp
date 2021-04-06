ba26b18c9a81eb759e0dd333a6e6fda60d16aef0
MasatoHidaka <m.hdk816@gmail.com>
2021-04-06 19:15:08 +0900

    experiment

--- ros_face_driver/src/base_driver.cpp
+++ ros_face_driver/src/base_driver.cpp
@@ -1,16 +1,19 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "ros_face_msgs/FaceCmd.h"
 
-class BaseDriver{
-	private:
-		ros::NodeHandle n;
-		ros::Publisher pub;
-		ros::Subscriber sub;
-		std_msgs::String send_cmd;
-	public:
-		BaseDriver();
-		void Publication(std_msgs::String buf);
-		void Callback(const std_msgs::String &recieve_cmd);
+class BaseDriver
+{
+private:
+	ros::NodeHandle n;
+	ros::Publisher pub;
+	ros::Subscriber sub;
+	std_msgs::String send_cmd;
+
+public:
+	BaseDriver();
+	void Publication(std_msgs::String buf);
+	void Callback(const ros_face_msgs::FaceCmd &recieve_cmd);
 };
 
 BaseDriver::BaseDriver()
@@ -19,31 +22,32 @@ BaseDriver::BaseDriver()
 	sub = n.subscribe("face_cmd", 100, &BaseDriver::Callback, this);
 }
 
-
 void BaseDriver::Publication(std_msgs::String buf)
 {
 	send_cmd = buf;
-    printf("pub:%s\n", send_cmd.data.c_str());
+	printf("pub:%s\n", send_cmd.data.c_str());
 	pub.publish(send_cmd);
 }
 
-
 void BaseDriver::Callback(const std_msgs::String &recieve_cmd)
 {
-	printf("sub:%s\n", recieve_cmd.data.c_str());
+	/*
+	printf("sub:%s\n", recieve_cmd..c_str());
     BaseDriver::Publication(recieve_cmd);
+	*/
+	printf(reciev_cmd);
 }
 
-int main(int argc, char**argv)
+int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "base_driver");
-	
+
 	BaseDriver base_driver;
 
 	ros::Rate loop_rate(100);
-	while(ros::ok()){
-		
-        ros::spinOnce();
+	while (ros::ok())
+	{
+		ros::spinOnce();
 		loop_rate.sleep();
 	}
 }
